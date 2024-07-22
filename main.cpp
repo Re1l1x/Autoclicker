d8749b22298d6692af7705d2aec24f3cb08a9e17
Re1l1x <reilixtech@gmail.com>
2024-07-22 19:48:03 +0400

    minifix

--- main.cpp
+++ main.cpp
@@ -8,15 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "Autoclicker_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
     MainWindow w;
     w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     w.setFixedSize(300, 300);
